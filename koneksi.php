c6cd2c6e3a83b0c17fe18d536f2984c91560f8ff
Muhakbarcom <muhammad.akbar5999@gmail.com>
2022-02-04 14:21:26 +0700

    edit database

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host = "sql6.freemysqlhosting.net";
 $user = "sql6470314";
 $pass = "TJkUCYL6kh";
-$name = "sig_stadion";
+$name = "sql6470314";
 
 $koneksi = mysqli_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
 mysqli_select_db($koneksi, $name) or die("Tidak ada database yang dipilih!");
